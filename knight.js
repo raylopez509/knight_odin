48d9d0b288605752c9bd51f05c75ae88478630a5
Ray Lopez <raylopez509@gmail.com>
2025-03-10 17:02:49 -0700

    set up gameboard with nodes Each node now has an array with the moves it can go to

--- knight.js
+++ knight.js
@@ -20,29 +20,21 @@ function getAdjacentMoves(node) {
   for (let i = 0; i < moves.length; i++) {
     let moveX = node.coordinates[0] + moves[i][0];
     let moveY = node.coordinates[1] + moves[i][1];
-    if (moveX > 0 && moveX < 7 && moveY > 0 && moveY < 7) {
+    if (moveX >= 0 && moveX <= 7 && moveY >= 0 && moveY <= 7) {
       adjacentMoves.push([moveX, moveY]);
     }
   }
   node.adjacentMoves = adjacentMoves;
 }
 
-let test = [3, 4];
-let num = [-2, -1];
-
-console.log(test[0] + num[0]);
-
-let node = new Node([0, 0]);
-getAdjacentMoves(node);
-
-console.log(node.adjacentMoves);
+const gameBoard = new Array(8);
 
 function createNodes() {
-  let gameBoard = new Array(8);
   for (let x = 0; x < 8; x++) {
     let yCoordinate = new Array(8);
     for (let y = 0; y < 8; y++) {
       let node = new Node([x, y]);
+      getAdjacentMoves(node);
       yCoordinate[y] = node;
     }
     gameBoard[x] = yCoordinate;
@@ -50,3 +42,12 @@ function createNodes() {
 }
 
 createNodes();
+
+console.log(gameBoard[0]);
+
+
+let test = new Array(8);
+
+test[0] = 5;
+
+console.log(test[0])
